f976d77014b80f42500922b9ba804701dcce3325
issuh <yoyus5@naver.com>
2020-07-15 23:06:54 +0900

    try repectoring message

--- include/srpc/Message.hpp
+++ include/srpc/Message.hpp
@@ -9,6 +9,7 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 namespace srpc {
 
@@ -26,8 +27,12 @@ enum class NET_COMMAND : uint8_t {
 struct HeaderPacket {
   uint8_t command;
   uint8_t reserve;
-  uint8_t offset;
   uint8_t headerSize;
+  uint8_t offset;
+  uint8_t serviceNameLen;
+  uint8_t rpcNameLen;
+  uint8_t serializedJsonLen;
+  uint8_t offset;
   uint32_t bodySize;
   uint32_t bodyIndex;
   uint32_t bodyLen;
@@ -37,10 +42,6 @@ struct HeaderPacket {
 
 #pragma pack(push, 1)
 struct BodyPacket {
-  uint8_t serviceNameLen;
-  uint8_t rpcNameLen;
-  uint8_t argCount;
-  uint8_t offset;
   std::vector<char> serviceName;
   std::vector<char> rpcName;
   std::vector<char> serializedJson;
@@ -57,19 +58,38 @@ class Message {
   size_t getHeaderSize() const { return sizeof(m_header); }
   size_t getBodySize() const { return sizeof(m_body); }
 
-  void setHeader() {}
-  void setBody() {}
-
   HeaderPacket getHeader() const { return m_header; }
   BodyPacket getBody() const { return m_body; }
 
-  const std::string& getSerializedHeader() const {
+  const std::string& serializedHeader() const {
     std::string str(reinterpret_cast<const char*>(&m_header));
     return str;
   }
-  const std::string& getSerializedBody() const {
-    std::string str(reinterpret_cast<const char*>(&m_body));
+  const std::string& serializedBody() const {
+    std::string str;
+    str.append(&m_body.serviceName[0]);
+    str.append(&m_body.rpcName[0]);
+    str.append(&m_body.serializedJson[0]);
+
     return str;
+  }
+
+  void deserializeHeader(const std::string& header) {
+    m_header = *reinterpret_cast<HeaderPacket*>(const_cast<char*>(header.c_str()));
+  }
+
+  void deserializeBody(const std::string& body) {
+    uint32_t start = 0;
+    uint32_t end = m_header.serviceNameLen;
+
+    std::copy(body.begin() + start, body.begin() + (start + end) + 1, std::back_inserter(m_body.rpcName));
+
+    start = end + 1;
+    end = m_header.rpcNameLen;
+    std::copy(body.begin() + start, body.begin() + (start + end) + 1, std::back_inserter(m_body.serviceName));
+
+    start = end + 1;
+    std::copy(body.begin() + start, body.end(), std::back_inserter(m_body.serializedJson));
   }
 
  private:
